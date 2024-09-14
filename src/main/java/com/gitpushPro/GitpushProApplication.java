aa59b9dbc2506c1f35eca2d252c7b277dbef50f9
sreeapurva <sreeapurva.rajasekharuni@team.telstra.com>
2024-09-14 11:16:35 +0530

    Added CSV file

--- src/main/java/com/gitpushPro/GitpushProApplication.java
+++ src/main/java/com/gitpushPro/GitpushProApplication.java
@@ -76,7 +76,7 @@ public class GitpushProApplication {
 
 			// Push to the GitLab
 			git.push()
-					.setCredentialsProvider(new UsernamePasswordCredentialsProvider(userName, password))
+					.setCredentialsProvider(new UsernamePasswordCredentialsProvider("apurvaaa12", "Apurva@1225"))
 					.setRemote("https://github.com/apurvaaa12/gitPushPro.git")
 					.call();
 			logger.info("Pushed the code to git");
